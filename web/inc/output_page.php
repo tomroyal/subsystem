eedef20821238799af36b27fcab26c3b4ccce9dd
Tom Royal <tom@tomroyal.com>
2016-09-18 08:47:21 +0100

    started setup script 2

---
+++ web/inc/output_page.php
@@ -0,0 +1,67 @@
+<?php
+
+// output a page with heading and content (in array $page_out) using the template
+
+// set some defaults if not known
+if ($ss_company == ""){
+	$ss_company = "SubSystem Payments";	
+};
+if ($ss_footer == ""){
+	$ss_footer = "Powered by SubSystem Payments";	
+};
+
+// top
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title><?echo($ss_company);?></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="./inc/css/normalize.css">
+  <link rel="stylesheet" href="./inc/css/skeleton.css">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container">
+	  	<div class="twelve columns">
+			<h1 class="remove-bottom" style="margin-top: 40px"><?echo($ss_company);?></h1>
+			<hr />
+		</div>
+
+    
+		<div class="twelve columns u-pull-left">	
+		
+<?php
+
+// content
+echo('<h3>'.$page_out[1].'</h3>');	
+echo('<p>'.$page_out[2].'</p>');			
+
+// bottom
+
+?>
+		<div class="u-cf">&nbsp;</div>
+		<div class="twelve columns">
+			<hr />
+			<p><?echo($ss_footer);?></p>
+			<p>All payments are processed securely using <a href="https://stripe.com">Stripe</a>. No card details are stored on this server.</p> 
+		</div>
+	</div><!-- container-->
+</body>
+</html>
+
+<?php
+
+?>
--- web/setup/index.php
+++ web/setup/index.php
@@ -10,10 +10,30 @@ $rs1 = pg_query($con, 'SELECT * FROM '.$schemaname.'.config');
 
 if (pg_num_rows($rs1) != 1){
 	// no config row, so not yet setup
-	if ($_GET['s'] != 1){
+	if ($_POST['s'] != 1){
 		// not yet sent form
-		
-		
+		$page_form = '
+			<form action="#" method="post">
+			<input type="hidden" name="s" value="1">
+			<div class="form-row">
+                <label>Your Email Address</label>
+                <input type="email" size="40" autocomplete="off" name="e1" />
+            </div>
+            <div class="form-row">
+                <label>Choose a Password</label>
+                <input type="password" size="40" autocomplete="off" name="p1" />
+            </div>
+            <div class="form-row">
+                <label>Confirm Password</label>
+                <input type="password" size="40" autocomplete="off" name="p2" />
+            </div>
+            <div class="form-row">
+                <label>Company or Business Name</label>
+                <input type="text" size="40" autocomplete="off" name="c1" />
+            </div>
+            <button type="submit" class="submit-button">Save Settings</button>
+		'; 
+		$page_out = array('SubSystem Setup',$page_form);		
 	}	
 	else {
 		// received form, process
@@ -26,6 +46,7 @@ else {
 
 
 // handle $page_out
+include './../inc/output_page.php';
 
 // close db
 pg_close($con);
