6cc888ecaf54765d49a4f7a8ea3ca3d15feacba2
Tom Royal <tom@tomroyal.com>
2016-09-18 10:28:02 +0100

    basic template for index

--- web/index.php
+++ web/index.php
@@ -2,4 +2,14 @@
 
 // public index, to come
 
+// connect to db
+include './inc/database.php';
+
+$page_out = array('Welcome','This is the home page tbc.');		
+
+// handle $page_out
+include './inc/output_page.php';
+
+// close db
+pg_close($con);
 ?>
